d75abb96461288915cc446c4076ac92263594b40
lefteriskat <lefteriskatiforis@gmail.com>
2023-06-11 19:16:06 +0200

    calculate_class weights

--- src/data/utils.py
+++ src/data/utils.py
@@ -21,3 +21,23 @@ def compute_specificity(predicted_mask, ground_truth):
 def compute_sensitivity(predicted_mask, ground_truth):
     sensitivity = torchmetrics.functional.classification.binary_recall(predicted_mask, ground_truth)
     return sensitivity
+
+def calculate_class_weights(dataset):
+    class_counts = [0,0]
+
+    # Step 1: Compute class frequencies
+    for _, label_batch in dataset:
+        values, counts = np.unique(label_batch, return_counts=True)
+        print(values)
+        print(counts)
+        class_counts = counts
+
+    # Step 2: Calculate class weights
+    total_samples = sum(class_counts)
+    class_weights = [total_samples / count for count in class_counts]
+
+    # Optional: Normalize the weights
+    class_weights = torch.tensor(class_weights)
+    class_weights = class_weights / class_weights.sum()
+
+    return class_weights
